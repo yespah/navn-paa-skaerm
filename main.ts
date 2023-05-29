5d61e361a90ace3acfd19f518267bf462a128667
Jesper Schleiss <yespah@users.noreply.github.com>
2023-05-29 13:45:28 +0200

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,41 +5,13 @@ basic.showLeds(`
     # . # . #
     # . # . #
     `)
-basic.showLeds(`
-    # . . . #
-    # # . . #
-    # . # . #
-    # . . # #
-    # . . . #
-    `)
-basic.showLeds(`
-    . # # # .
-    . . . # .
-    . # # # .
-    . # . # .
-    . # # # .
-    `)
-basic.showLeds(`
-    . . . . .
-    . # # # .
-    . # . # .
-    . # . # .
-    . # # # .
-    `)
-basic.showLeds(`
-    # . . . .
-    # # # # #
-    # . # . #
-    # . # . #
-    # . # . #
-    `)
-basic.showLeds(`
-    . . # . .
-    . . . . .
-    . . # . .
-    . . # . .
-    . . # . .
-    `)
+basic.showString("Navn")
 music.playMelody("G B A G C5 B A B ", 120)
 basic.showIcon(IconNames.Heart)
-TobbieII.dance(7)
+TobbieII.rightward()
+TobbieII.shake_head(5)
+TobbieII.stopturn()
+TobbieII.leftward()
+TobbieII.vibrate(5)
+TobbieII.stopturn()
+TobbieII.dance(6)
